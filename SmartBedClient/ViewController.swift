6ad2a472c79728550ec4277e510c3e6d6fdcda8b
OrbitalSqwib <orbitalsqwib@gmail.com>
2020-02-02 23:22:24 +0800

    Initial Commit

---
+++ SmartBedClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartBedClient
+//
+//  Created by Eugene L. on 2/2/20.
+//  Copyright © 2020 ARandomDeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
